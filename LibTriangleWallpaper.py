d6af52a4c57e1fa12e94f979fc6f83a0da0815f3
Matthew Sullivan <elephant454@gmail.com>
2016-04-21 21:48:27 -0700

    Formatted code to better match PEP8 standards. More changes have to be made, but this is a start.

--- LibTriangleWallpaper.py
+++ LibTriangleWallpaper.py
@@ -2,87 +2,102 @@
 import math, numpy, random, copy
 from PIL import Image, ImageDraw
 
+
 class Tessellation:
-    def __init__(self, width, height, targetVerts):
+    def __init__(self, width, height, target_verts):
         self.verts = []
         self.width = width
         self.height = height
-        self.xVerts = (round(math.sqrt((width*targetVerts)/height))-1) #The number of verticies in one row in order to be spread evenly
-        self.yVerts = (round(math.sqrt((height*targetVerts)/width))-1) #The number of verticies in each column in order to be spread evenly
-        xdel = (width-1) / (self.xVerts-1)
-        ydel = (height-1) / (self.yVerts-1)
+        # The number of vertices in one row in order to be spread evenly
+        self.xVerts = round(math.sqrt((width * target_verts) / height)) - 1
+
+        # The number of vertices in each column in order to be spread evenly
+        self.yVerts = round(math.sqrt((height * target_verts) / width)) - 1
+        xdel = (width - 1) / (self.xVerts - 1)
+        ydel = (height - 1) / (self.yVerts - 1)
         for y in numpy.arange(0, self.yVerts):
             for x in numpy.arange(0, self.xVerts):
-                self.verts.append((x*xdel, y*ydel))
+                self.verts.append((x * xdel, y * ydel))
 
-def getImageColors(image, width, height): #get a list of colors from an image
-    return(list(image.resize([width,height], Image.LANCZOS).getdata()))
 
-def drawTriangleFan(tes, colors):
+def get_image_colors(image, width, height):  # get a list of colors from an image
+    return list(image.resize([width, height], Image.LANCZOS).getdata())
+
+
+def draw_triangle_fan(tes: Tessellation, colors: object) -> object:
     index = 0
-    result = Image.new("RGB", (int(tes.verts[len(tes.verts)-1][0]+1), int(tes.verts[len(tes.verts)-1][1]+1)))
+    result = Image.new("RGB", (int(tes.verts[len(tes.verts) - 1][0] + 1), int(tes.verts[len(tes.verts) - 1][1] + 1)))
     draw = ImageDraw.Draw(result)
-    for i in range(0, len(tes.verts)-tes.xVerts):
-        if (i+1)%tes.xVerts == 0: continue
-        draw.polygon([tes.verts[i], tes.verts[i+tes.xVerts], tes.verts[i+tes.xVerts+1]], fill=colors[index])
+    for i in range(0, len(tes.verts) - tes.xVerts):
+        if (i + 1) % tes.xVerts == 0: continue
+        draw.polygon([tes.verts[i], tes.verts[i + tes.xVerts], tes.verts[i + tes.xVerts + 1]], fill=colors[index])
         index += 1
-        draw.polygon([tes.verts[i], tes.verts[i+1], tes.verts[i+tes.xVerts+1]], fill=colors[index])
+        draw.polygon([tes.verts[i], tes.verts[i + 1], tes.verts[i + tes.xVerts + 1]], fill=colors[index])
         index += 1
     del draw
-    return(result)
+    return result
+
 
 def randomizeTessellation(tes):
     newVerts = []
     result = copy.deepcopy(tes)
     for i in range(0, len(tes.verts)):
-        if i<tes.xVerts:
+        if i < tes.xVerts:
             newVerts.append(tes.verts[i])
             True
-        elif i>len(tes.verts)-tes.xVerts:
+        elif i > len(tes.verts) - tes.xVerts:
             newVerts.append(tes.verts[i])
             True
-        elif i%tes.xVerts == 0:
+        elif i % tes.xVerts == 0:
             newVerts.append(tes.verts[i])
             True
-        elif (i+1)%tes.xVerts == 0:
+        elif (i + 1) % tes.xVerts == 0:
             newVerts.append(tes.verts[i])
             True
         else:
-            newVerts.append((tes.verts[i][0] - (tes.width/(tes.xVerts-1)/2) + (tes.width/(tes.xVerts-1))*random.random()/2, tes.verts[i][1] - (tes.height/(tes.yVerts-1)/2) + (tes.height/(tes.yVerts-1))*random.random()/2))
+            newVerts.append((tes.verts[i][0] - (tes.width / (tes.xVerts - 1) / 2) + (
+            tes.width / (tes.xVerts - 1)) * random.random() / 2,
+                             tes.verts[i][1] - (tes.height / (tes.yVerts - 1) / 2) + (
+                             tes.height / (tes.yVerts - 1)) * random.random() / 2))
     result.verts = newVerts
-    return(result)
+    return (result)
 
 
 def distance(a, b):
-    return(math.sqrt(((b[0]-a[0])**2) + ((b[1]-a[1])**2)))
+    return (math.sqrt(((b[0] - a[0]) ** 2) + ((b[1] - a[1]) ** 2)))
+
 
 def pullTessellation(center, verts):
     result = []
     for i in range(0, len(verts)):
-        if i<xVerts:
+        if i < xVerts:
             result.append(verts[i])
-        elif i>len(verts)-xVerts:
+        elif i > len(verts) - xVerts:
             result.append(verts[i])
-        elif i%xVerts == 0:
+        elif i % xVerts == 0:
             result.append(verts[i])
-        elif (i+1)%xVerts == 0:
+        elif (i + 1) % xVerts == 0:
             result.append(verts[i])
         else:
 
-            print("X: " + (str)(verts[i][0]) + " + (" + (str)(verts[center][0]) + " - " + (str)(verts[i][0]) + ") = " + (str)(verts[i][0] + (verts[center][0] - verts[i][0])/3))
-            print("Y: " + (str)(verts[i][1]) + " + (" + (str)(verts[center][1]) + " - " + (str)(verts[i][1]) + ") = " + (str)(verts[i][1] + (verts[center][1] - verts[i][1])/3))
-            result.append((verts[i][0] + (verts[center][0] - verts[i][0]), verts[i][1] + (verts[center][1] - verts[i][1])))
+            print(
+                "X: " + (str)(verts[i][0]) + " + (" + (str)(verts[center][0]) + " - " + (str)(verts[i][0]) + ") = " + (
+                str)(verts[i][0] + (verts[center][0] - verts[i][0]) / 3))
+            print(
+                "Y: " + (str)(verts[i][1]) + " + (" + (str)(verts[center][1]) + " - " + (str)(verts[i][1]) + ") = " + (
+                str)(verts[i][1] + (verts[center][1] - verts[i][1]) / 3))
+            result.append(
+                (verts[i][0] + (verts[center][0] - verts[i][0]), verts[i][1] + (verts[center][1] - verts[i][1])))
 
-    return(result)
+    return (result)
 
-
-#gen = genTessellation()
-#for i in range(0, 10):
+# gen = genTessellation()
+# for i in range(0, 10):
 #    gen = randomizeTessellation(gen)
-#drawTriangleFan(gen)
+# drawTriangleFan(gen)
 
-#tes = genTessellation()
-#center = round(random.random()*len(tes))
-#drawTriangleFan(pullTessellation(center, tes)).show()
+# tes = genTessellation()
+# center = round(random.random()*len(tes))
+# drawTriangleFan(pullTessellation(center, tes)).show()
 
-#drawTriangleFan(randomizeTessellation(genTessellation(original.width, original.height, targetVerts)), getImageColors(original)).save("output." + original.filename.rpartition('.')[2], original.format)
+# drawTriangleFan(randomizeTessellation(genTessellation(original.width, original.height, targetVerts)), getImageColors(original)).save("output." + original.filename.rpartition('.')[2], original.format)
--- TriangleWallpaper.py
+++ TriangleWallpaper.py
@@ -5,7 +5,8 @@ import LibTriangleWallpaper as ltw
 
 parser = argparse.ArgumentParser(description="Turn an image into triangles.")
 parser.add_argument("file", type=open, help="image file to turn into triangles")
-parser.add_argument("-v", type=int, default=2048, metavar="verticies", help="number of verticies that the picture should aim to have (default is 2048)")
+parser.add_argument("-v", type=int, default=2048, metavar="verticies",
+                    help="number of verticies that the picture should aim to have (default is 2048)")
 args = parser.parse_args()
 
 original = Image.open(args.file.name)
@@ -13,13 +14,18 @@ original = Image.open(args.file.name)
 
 width = original.width
 height = original.height
-targetVerts = args.v #The number of verticies to aim for. The actual number may be less.
-#targetVerts = 2048 #The number of verticies to aim for. The actual number may be less.
-xVerts = (round(math.sqrt((width*targetVerts)/height))-1) #The number of verticies in one row in order to be spread evenly
-yVerts = (round(math.sqrt((height*targetVerts)/width))-1) #The number of verticies in each column in order to be spread evenly
+
+# The number of vertices to aim for. The actual number may be less.
+targetVerts = args.v
+#targetVerts = 2048
+
+# The number of vertices in each row or column in order to be spread vertices evenly
+xVerts = (round(math.sqrt((width*targetVerts)/height))-1)
+yVerts = (round(math.sqrt((height*targetVerts)/width))-1)
 
 tes = ltw.Tessellation(original.width, original.height, targetVerts)
 print(len(tes.verts))
 tes = ltw.randomizeTessellation(tes)
 print(len(tes.verts))
-ltw.drawTriangleFan(tes, ltw.getImageColors(original, (xVerts-1)*2, (yVerts-1))).save("output." + original.filename.rpartition('.')[2], original.format)
+ltw.draw_triangle_fan(tes, ltw.get_image_colors(original, (xVerts-1)*2, (yVerts-1))).save(
+    "output." + original.filename.rpartition('.')[2], original.format)
