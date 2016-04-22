69d0df31add0c3b455bbf0b0ec2791b851416413
Matthew Sullivan <elephant454@gmail.com>
2016-04-22 14:02:26 -0700

    LibTriangleWallpaper.py will take arguments and produce output when run as a script, but can still be imported to work with its individual functions. The TriangleWallpaper.py file will be replaced.

--- LibTriangleWallpaper.py
+++ LibTriangleWallpaper.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import argparse
 import copy
 import math
 import numpy
@@ -96,3 +97,29 @@ def pull_tessellation(tes: Tessellation, center: tuple):
                  (tes.vertices[center][1] - tes.vertices[i][1])))
 
     return result
+
+
+# if the module is ran as a script, it will take arguments and produce output
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Turn an image into triangles.")
+    parser.add_argument("file", type=open, help="image file to turn into triangles")
+    parser.add_argument("-v", type=int, default=2048, metavar="verticies",
+                        help="number of vertices that the picture should aim to have (default is 2048)")
+    args = parser.parse_args()
+
+    original = Image.open(args.file.name)
+
+    width = original.width
+    height = original.height
+
+    # The number of vertices to aim for. The actual number may be less.
+    targetVerts = args.v
+
+    # The number of vertices in each row or column in order to be spread vertices evenly
+    xVerts = (round(math.sqrt((width*targetVerts)/height))-1)
+    yVerts = (round(math.sqrt((height*targetVerts)/width))-1)
+
+    tes = Tessellation(original.width, original.height, targetVerts)
+    tes = randomize_tessellation(tes)
+    draw_triangle_fan(tes, get_image_colors(original, (xVerts - 1) * 2, (yVerts - 1))).save(
+        "output." + original.filename.rpartition('.')[2], original.format)
--- TriangleWallpaper.py
+++ TriangleWallpaper.py
@@ -3,29 +3,26 @@ import argparse, math
 from PIL import Image
 from LibTriangleWallpaper import Tessellation, get_image_colors, randomize_tessellation, draw_triangle_fan
 
-parser = argparse.ArgumentParser(description="Turn an image into triangles.")
-parser.add_argument("file", type=open, help="image file to turn into triangles")
-parser.add_argument("-v", type=int, default=2048, metavar="verticies",
-                    help="number of verticies that the picture should aim to have (default is 2048)")
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Turn an image into triangles.")
+    parser.add_argument("file", type=open, help="image file to turn into triangles")
+    parser.add_argument("-v", type=int, default=2048, metavar="verticies",
+                        help="number of verticies that the picture should aim to have (default is 2048)")
+    args = parser.parse_args()
 
-original = Image.open(args.file.name)
-#original = Image.open("fenstack.jpg")
+    original = Image.open(args.file.name)
 
-width = original.width
-height = original.height
+    width = original.width
+    height = original.height
 
-# The number of vertices to aim for. The actual number may be less.
-targetVerts = args.v
-#targetVerts = 2048
+    # The number of vertices to aim for. The actual number may be less.
+    targetVerts = args.v
 
-# The number of vertices in each row or column in order to be spread vertices evenly
-xVerts = (round(math.sqrt((width*targetVerts)/height))-1)
-yVerts = (round(math.sqrt((height*targetVerts)/width))-1)
+    # The number of vertices in each row or column in order to be spread vertices evenly
+    xVerts = (round(math.sqrt((width*targetVerts)/height))-1)
+    yVerts = (round(math.sqrt((height*targetVerts)/width))-1)
 
-tes = Tessellation(original.width, original.height, targetVerts)
-print(len(tes.vertices))
-tes = randomize_tessellation(tes)
-print(len(tes.vertices))
-draw_triangle_fan(tes, get_image_colors(original, (xVerts - 1) * 2, (yVerts - 1))).save(
-    "output." + original.filename.rpartition('.')[2], original.format)
+    tes = Tessellation(original.width, original.height, targetVerts)
+    tes = randomize_tessellation(tes)
+    draw_triangle_fan(tes, get_image_colors(original, (xVerts - 1) * 2, (yVerts - 1))).save(
+        "output." + original.filename.rpartition('.')[2], original.format)
