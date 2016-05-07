2a7c328abdde0550841563b2993390dfc6ccc825
Matthew Sullivan <elephant454@gmail.com>
2016-05-06 17:21:35 -0700

    did some additional random color generation things (cherry picked from commit 447b17a)

--- TriangleWallpaper.py
+++ TriangleWallpaper.py
@@ -103,7 +103,7 @@ def pull_tessellation(tes: Tessellation, center: tuple):
 # if the module is ran as a script, it will take arguments and produce output
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Turn an image into triangles.")
-    parser.add_argument("file", type=open, help="image file to turn into triangles")
+    parser.add_argument("-i", "--file", type=open, help="image file to turn into triangles")
     parser.add_argument("-v", type=int, default=2048, metavar="vertices",
                         help="number of vertices that the picture should aim to have (default is 2048)")
     parser.add_argument("--no-randomization", dest="randomize", action="store_false", help="don't randomize vertices")
